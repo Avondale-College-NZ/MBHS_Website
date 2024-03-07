b8dabd293acf579fd6a6cbcf9e4d57a917455dab
Saaid Faraz <farazsaaid@gmail.com>
2024-03-07 13:28:42 +1300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MBHS_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
