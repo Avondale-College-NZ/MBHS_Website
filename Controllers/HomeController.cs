7031a9e97fd8d8e5ae849ad58bc4ac5b998b301a
Saaid Faraz <farazsaaid@gmail.com>
2024-04-19 19:10:07 +1200

    Added Footer.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace MBHS_Website.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
