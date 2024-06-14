0a442a1bbd979031dc5abfeeb4e85a688612f55f
ac117413 <ac117413@avcol.school.nz>
2024-06-14 14:05:16 +1200

    Added Edit users view.

---
+++ Models/EditUserViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace MBHS_Website.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+   
+        public class EditUserViewModel
+        {
+            //To avoid NullReferenceExceptions at runtime,
+            //initialise Claims and Roles with a new list in the constructor.
+            public EditUserViewModel()
+            {
+                Roles = new List<string>();
+            }
+
+            [Required]
+            public string Id { get; set; }
+
+            [Required]
+            [Display(Name = "User Name")]
+            public string UserName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            public DateTime DateOfBirth { get; set; }
+
+            public IList<string> Roles { get; set; }
+        }
+ }
+
